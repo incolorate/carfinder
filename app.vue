0e054ccbea3d6056765f81b81c8a68e07797f051
incolorate <negrupaul59@gmail.com>
2024-01-13 19:45:18 +0200

    Navbar added

--- app.vue
+++ app.vue
@@ -1,9 +1,8 @@
+<script setup lang="ts"></script>
+
 <template>
-  <header>
-    <div
-      class="text-6xl text-slate-900 p-4 z-50 bg-slate-100 shadow-lg sticky top-0"
-    >
-      <NuxtLink to="/">carfinder</NuxtLink>
-    </div>
-  </header>
+  <div>
+    <Navbar />
+    <NextPage />
+  </div>
 </template>
