e82745992299e05a85e4c9c0ecd56768bc3ca254
incolorate <negrupaul59@gmail.com>
2024-01-13 10:30:33 +0200

    Project setup

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <div
+      class="text-6xl text-slate-900 p-4 z-50 bg-slate-100 shadow-lg sticky top-0"
+    >
+      <NuxtLink to="/">carfinder</NuxtLink>
+    </div>
+  </header>
+</template>
