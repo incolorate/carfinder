8cff1e7f20215bec452b6e67e14c17eb9fc7fe92
incolorate <negrupaul59@gmail.com>
2024-01-15 17:38:09 +0200

    listing page

---
+++ components/car/ListingCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  listing: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="shadow rounded overflow-hidden flex justify-between mb-4">
+    <div class="flex">
+      <img :src="listing.url" alt="" class="w-80 mr-3 h44" />
+      <div class="p-3">
+        <h1 class="text-2xl">{{ listing.name }}</h1>
+        <p class="text-blue-500">${{ listing.price }}</p>
+      </div>
+    </div>
+    <div class="p-3 flex">
+      <NuxtLink
+        class="text-blue-400 mr-5"
+        :to="`profile/listing/view/${listing.id}`"
+        >View</NuxtLink
+      >
+      <p class="text-red-500 cursor-pointer">Delete</p>
+    </div>
+  </div>
+</template>
---
+++ pages/profile/listings/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+
+const { listings } = useCars();
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-between mt-24 items-center">
+      <h1 class="text-xl">My listings</h1>
+      <NuxtLink
+        to="profile/listings/create"
+        class="text-xl text-blue-500 w-9 h-9"
+        >+</NuxtLink
+      >
+    </div>
+    <div class="shadow rounded p-3 mt-5">
+      <CarListingCard
+        v-for="listing in listings"
+        :key="listing.id"
+        :listing="listing"
+      />
+    </div>
+  </div>
+</template>
