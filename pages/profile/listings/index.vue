b6c02f31ccc70466594ae2af2bceffdc076e5ff2
incolorate <negrupaul59@gmail.com>
2024-01-17 10:14:50 +0200

    Prisma, db schema and an post endpoint created

--- pages/profile/listings/index.vue
+++ pages/profile/listings/index.vue
@@ -11,9 +11,7 @@ const { listings } = useCars();
   <div>
     <div class="flex justify-between mt-24 items-center">
       <h1 class="text-xl">My listings</h1>
-      <NuxtLink
-        to="profile/listings/create"
-        class="text-xl text-blue-500 w-9 h-9"
+      <NuxtLink to="listings/create" class="text-xl text-blue-500 w-9 h-9"
         >+</NuxtLink
       >
     </div>
---
+++ server/api/car/listings/index.post.js
@@ -0,0 +1,66 @@
+import Joi from "joi";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const schema = Joi.object({
+  make: Joi.string().required(),
+  model: Joi.string().required(),
+  year: Joi.number()
+    .min(1886)
+    .max(new Date().getFullYear() + 1),
+  miles: Joi.number(),
+  city: Joi.string().min(2).required(),
+  numberOfSeats: Joi.number().min(1).max(1000).required(),
+  features: Joi.array().items(Joi.string()).required(),
+  image: Joi.string().required(),
+  listerId: Joi.string().required(),
+  price: Joi.number().min(0).required(),
+  name: Joi.string().required(),
+  description: Joi.string().required().min(20),
+});
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const { error, value } = await schema.validate(body);
+
+  if (error) {
+    throw createError({
+      statusCode: 412,
+      message: error.message,
+    });
+  }
+
+  const {
+    make,
+    model,
+    miles,
+    city,
+    numberOfSeats,
+    features,
+    image,
+    listerId,
+    price,
+    name,
+    description,
+  } = body;
+
+  const car = await prisma.car.create({
+    data: {
+      make,
+      model,
+      miles,
+      city: city.toLowerCase(),
+      numberOfSeats,
+      features,
+      image,
+      listerId,
+      price,
+      name,
+      description,
+      year: 2009,
+    },
+  });
+
+  return car;
+});
